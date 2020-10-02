07fdc719365a9f5710531aaeeb48a3d18ee12162
KSaxena <kshitijsaxena01@gmail.com>
2020-10-02 19:32:11 +0530

    Index Page Updated

---
+++ js/main.js
@@ -0,0 +1,36 @@
+function changein(a){
+	var x = document.getElementsByTagName("body");
+	if(a.id == 'eat')
+	{
+		x[0].style.backgroundImage = "url('./images/main_background1.jpeg')";
+		a.style.borderBottom = "3px solid #ffffff";
+	}
+	if(a.id == 'drink')
+	{
+		x[0].style.backgroundImage = "url('./images/main_background2.jpg')";
+		a.style.borderBottom = "3px solid #ffffff";
+	}
+	if(a.id == 'visit')
+	{
+		x[0].style.backgroundImage = "url('./images/main_background3.jpg')";
+		a.style.borderBottom = "3px solid #ffffff";
+	}
+}
+function changeout(a){
+	var x = document.getElementsByTagName("body");
+	if(a.id == 'eat')
+	{
+		x[0].style.backgroundImage = "url('./images/main_background.jpg')";
+		a.style.borderBottom = "none";
+	}
+	if(a.id == 'drink')
+	{
+		x[0].style.backgroundImage = "url('./images/main_background.jpg')";
+		a.style.borderBottom = "none";
+	}
+	if(a.id == 'visit')
+	{
+		x[0].style.backgroundImage = "url('./images/main_background.jpg')";
+		a.style.borderBottom = "none";
+	}
+}
