5704a1333ff5ed51588347238e26ba4e283307d1
KSaxena <kshitijsaxena01@gmail.com>
2020-10-03 19:18:37 +0530

    Responsive Nav Bar

--- js/main.js
+++ js/main.js
@@ -34,3 +34,10 @@ function changeout(a){
 		a.style.borderBottom = "none";
 	}
 }
+function slide(){
+	const burger = document.getElementsByClassName('burger');
+	const nav = document.getElementsByClassName('nav-links');
+	nav[0].classList.toggle('nav-active');
+	burger[0].classList.toggle('toggle');
+
+}
